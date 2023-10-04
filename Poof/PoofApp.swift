32a000f260e1acf8401b7efa9caa70d365129908
Jonathan Evan <jonathanevanc@gmail.com>
2023-10-04 14:27:53 +0700

    Initial Commit

---
+++ Poof/PoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoofApp.swift
+//  Poof
+//
+//  Created by Jonathan Evan Christian on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
