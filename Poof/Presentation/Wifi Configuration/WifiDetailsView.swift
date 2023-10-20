4036514a12b3493a84e6f5fae2c041df2f5bfee9
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-10-20 14:12:54 +0700

    Update FE for Wifi Configuration Features

--- Poof/Presentation/Wifi Configuration/WifiDetailsView.swift
+++ Poof/Presentation/Wifi Configuration/WifiDetailsView.swift
@@ -14,20 +14,65 @@ struct WiFiDetailsView: View {
 
     var body: some View {
         ZStack {
-            VStack(spacing: 20) {
-                TextField("SSID", text: $ssid)
-                    .padding()
-                    .border(Color.gray)
-                SecureField("Password", text: $password)
-                    .padding()
-                    .border(Color.gray)
-                Button("Post WiFi Details") {
-                    viewModel.postWiFiDetails(ssid: ssid, password: password)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Choose a Wi-Fi")
+                        .font(.systemTitle2)
+                    Text("Wi-Fi akan menjadi sumber koneksi internet untuk inhaler melakukan singkronisasi.")
+                        .font(.systemBodyText)
+                        .multilineTextAlignment(.center)
                 }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8)
+                
+                VStack (spacing: 20) {
+                    
+                    VStack(alignment: .leading, spacing:0) {
+                        HStack (spacing: 0){
+                                Text("SSID")
+                                Text("*")
+                                    .foregroundColor(Color.red)
+                            }
+                        TextField("Wi-Fi Name", text: $ssid)
+                            .padding()
+                            .background(
+                                        Rectangle()
+                                            .frame(height: 1)
+                                            .foregroundColor(Color.gray), alignment: .bottom
+                                    )
+                    }
+                   
+                    
+                    VStack (alignment: .leading, spacing:0) {
+                        HStack (spacing: 0){
+                                Text("Password")
+                                Text("*")
+                                    .foregroundColor(Color.red)
+                            }
+                        SecureField("Wi-Fi Password", text: $password)
+                            .padding()
+                            .background(
+                                        Rectangle()
+                                            .frame(height: 1)
+                                            .foregroundColor(Color.gray), alignment: .bottom
+                                    )
+                    }
+                    
+                    
+                }
+                Button(action: {
+                    viewModel.postWiFiDetails(ssid: ssid, password: password)
+                }) {
+                    Text("Post WiFi Details")
+                        .font(.systemButtonText)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.primary1)
+                        .cornerRadius(10)
+                        .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                }
+                
+                Spacer()
+
             }
             .padding()
 
